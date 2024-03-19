788db114183b4b20d530356ba450aa6416fae44b
2typed <ghastediscool@outlook.com>
2024-03-20 00:19:28 +0100

    Test Commit.

---
+++ src/test.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+
+int deathspeak::core::init() {
+    std::cout << "Initialized.";
+    return 0;
+}
---
+++ src/test.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+namespace deathspeak {
+    namespace core {
+        int init();
+    }
+}
